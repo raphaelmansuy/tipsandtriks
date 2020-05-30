071a64a3f18d8a00dc7fdda17382ff52b04e751a
Raphael MANSUY <raphael.mansuy@caliatys.com>
2020-05-30 17:16:52 +0200

    example with tailwindcss and next

---
+++ how-to-add-tailwindcss-to-next/pages/index.js
@@ -0,0 +1,8 @@
+/* import "../styles/index.css" */
+
+export default () => (
+  <div className="tp4 shadow rounded bg-white">
+    <h1 className="text-purple leading-normal">Next.js</h1>
+    <p className="text-grey-dark">with TailwindCSS</p>
+  </div>
+)
